2905fd0687de42af039c46f8e6c79718bbb3e73f
hưng <nphbo1998@gmail.com>
2020-07-17 22:41:18 +0700

    Updates

---
+++ precache-manifest.7556637a1f61f54b46aa00237e92799a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df9a91d984d13fa1f43959d413f7f6c",
+    "url": "/messenger-clone/index.html"
+  },
+  {
+    "revision": "573e142adceb7d79501b",
+    "url": "/messenger-clone/static/css/main.7df2eb3b.chunk.css"
+  },
+  {
+    "revision": "76e6fb45ec562ed76e16",
+    "url": "/messenger-clone/static/js/2.52b24a28.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/messenger-clone/static/js/2.52b24a28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "573e142adceb7d79501b",
+    "url": "/messenger-clone/static/js/main.576e7a08.chunk.js"
+  },
+  {
+    "revision": "714a4f467b9794a0a83a",
+    "url": "/messenger-clone/static/js/runtime-main.aa7ddd0c.js"
+  }
+]);
